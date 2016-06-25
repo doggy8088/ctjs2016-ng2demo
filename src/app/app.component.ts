ac1e31ab386c2541a4b01bc35174b7485359d2a1
Will <doggy.huang@gmail.com>
2016-06-25 09:30:00 +0800

    切割 HeaderComponent 與 ArticleComponent 元件
    ng g component header
    ng g component article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/';
+import { ArticleComponent } from './article/';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent, ArticleComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -33,6 +33,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/header',
+  'app/article',
   /** @cli-barrel */
 ];
 
